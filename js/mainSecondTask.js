1ae477d3a3fd121d800406d475a0dbe1d6d64d1b
InWalker07 <112712020+InWalker07@users.noreply.github.com>
2022-11-12 23:34:47 +0100

    Add files via upload

---
+++ js/mainSecondTask.js
@@ -0,0 +1,17 @@
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 1n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol() // "symbol"
+
+typeof {} // "object"
+
+typeof null // "object"
+
+typeof function(){} // "function"
